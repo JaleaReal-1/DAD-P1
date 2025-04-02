0dcab0bbbe4be544c430a2920880b23d8c1b2c71
JaleaReal-1 <jalpawel753@gmail.com>
2025-04-02 17:02:25 -0500

    eureka

---
+++ ms-registry-serve/src/test/java/com/example/msregistryserve/MsRegistryServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msregistryserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegistryServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
